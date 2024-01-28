e6dd35c00baaadb1f21e94db296666aa862c57b1
RasulbekQadomboyev <rasqadomboyev@gmail.com>
2024-01-29 00:39:26 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,4 +1,4 @@
 
-BOT_TOKEN = "6780604386:AAEEy1q2Nvp8WjqCranyizWNDwAcModvho4"  # Bot toekn
-ADMINS = ['984568970','6053330218','6750573622']  # adminlar ro'yxati
+BOT_TOKEN = ""  # Bot toekn
+ADMINS = ['']  # adminlar ro'yxati
 IP = "locolhost" # Xosting ip manzili
